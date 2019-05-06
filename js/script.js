a123a30063354df04183df5931d510a691fcdeda
Sarika <biwalkar.sarika@gmail.com>
2019-05-05 20:27:10 -0700

    Corrected the file structure

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var car = {
+			make: 'VM',
+			color: 'blue',
+			type: 'Polo',
+			isTurnedOn: false,
+			numberOfWheels: 4,
+			seats: ['Seat1','Seat2','Seat3','Seat 4'],
+			turnOn: function ()
+			{
+				this.isTurnedOn = true;
+			},
+			fly: function()
+			{
+				alert('fly');
+			},
+
+			switchCar: function(isOn)
+			{
+				console.log('turn car '+isOn);
+				if (isOn == true) {
+					this.isTurnedOn = true;
+
+				}
+				else
+					this.isTurnedOn = false;
+			}
+
+		};
+		console.log('Hello');
